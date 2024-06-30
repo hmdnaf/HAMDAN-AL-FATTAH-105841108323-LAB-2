049ac45db99d9d440cc8046dc1984dc68f22f6d1
Hamdan AF <110399823+hmdnaf@users.noreply.github.com>
2024-06-30 21:06:45 +0800

    kalkulator sederhana
    tugas pertama klo ga salah

---
+++ pertemuan 3 tugas 2/kalkulator sederhana/main.js
@@ -0,0 +1,33 @@
+function calculate(operation) {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    let result;
+
+    if (isNaN(num1) || isNaN(num2)) {
+        alert('Silakan masukkan angka yang valid.');
+        return;
+    }
+
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if (num2 === 0) {
+                alert('Tidak bisa membagi dengan nol.');
+                return;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            return;
+    }
+
+    document.getElementById('result').value = result;
+}
